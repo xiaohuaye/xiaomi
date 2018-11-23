7ac254964dcfcade3f0f3b1d3cca6e70552e787b
xiaohuaye <908347816@qq.com>
2018-11-24 04:12:45 +0800

    轮播让用户离开页面时停止

--- solidplay.js
+++ solidplay.js
@@ -1,17 +1,25 @@
 // 初始化
 var n = 1
+var indexLast 
 var imges = $('.imges>img').length
 $('.btns>span').append("<span></span>")
 $('.btns>span:nth-child(1)>span').addClass('square')
+
 // 
-var timer = setTimer(n)
+var timer = setTimer()
 $('.btns>span').on('click',function(){
     clearInterval(timer)
     let index = $(this).index()
     local(index)
-    setTimeout(()=>{
+    if (indexLast !== index){
+        indexLast = index
+        $('.btns>span').removeClass('mask')
+        $(this).addClass('mask')
+    }else if(indexLast === index){
+        $('.btns>span').removeClass('mask')
         timer = setTimer()
-    },10000)
+        indexLast = undefined
+    }
 })
 $('.window').on('mouseenter', function(){
     clearInterval(timer)
@@ -19,6 +27,15 @@ $('.window').on('mouseenter', function(){
 
 $('.window').on('mouseleave', function(){
     timer = setTimer()
+    $('.btns>span').removeClass('mask')
+    indexLast = undefined
+})
+document.addEventListener('visibilitychange', function() {
+    if(document.hidden){ //页面不可见状态
+        clearInterval(timer)
+    }else{ //页面可见状态
+        timer = setTimer()
+    }
 })
 
 //函数声明
@@ -26,10 +43,10 @@ function setTimer(){
     return setInterval(()=>{
         if(n === imges){n = 0}
        local(n)
-       console.log(n)
         n ++
     },3000)
 }
+
 function local(n){
     $('#imges').css('transform', 'translateX('+ (-868*n) + 'px)')
     $('#btns>span').css('border','4px solid transparent')
